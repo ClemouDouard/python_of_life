0fe2cc7a85527587325b6a7650efe3f714a557c6
ClemouDouard <clementleveque911@gmail.com>
2024-12-23 12:37:40 +0100

    finished the board

--- src/board.py
+++ src/board.py
@@ -2,15 +2,22 @@ class Board:
     def __init__(self, width: int, height: int) -> None:
         self.width = width
         self.height = height
-        self.cells = [[Cell for _ in range(width)] for _ in range(height)]
+
+    def __str__(self) -> str:
+        res = ""
+        for x in range (self.height):
+            res += "| "
+            for y in range (self.width):
+                res += f"{self.cells[x][y]} | "
+            res += "\n"
+        return res
 
     def fill_cells(self) -> None:
+        self.cells = []
         for i in range (self.height) :
+            self.cells.append([])
             for j in range (self.width) :
-                self.cells[i][j] = Cell(j, i, False, self)
-
-    def get_cells(self) -> list:
-        return self.cells
+                self.cells[i].append(Cell(j, i, False, self))
 
 class Cell:
 
@@ -18,18 +25,24 @@ class Cell:
         self.x = x
         self.y = y
         self.alive = alive
-        self.board = Board
+        self.board = b
+
+    def __str__(self) -> str:
+        if self.alive == True:
+            return "1"
+        else:
+            return "0"
 
     def calc_neighbors(self) -> None:
-            self.neighbors = []
-            for dy in [-1, 0, 1]:
-                for dx in [-1, 0, 1]:
-                    if dx == 0 and dy == 0:
-                        continue
+        self.neighbors = []
+        for dy in [-1, 0, 1]:
+            for dx in [-1, 0, 1]:
+               if dx == 0 and dy == 0:
+                   continue
 
-                    nx, ny = self.x + dx, self.y + dy
+               nx, ny = self.x + dx, self.y + dy
 
-                    # verify if in limit
-                    if 0 <= nx < self.board.width and 0 <= ny < self.board.height:
-                        neighbor = self.board.get_cells()[ny][nx]
-                        self.neighbors.append(neighbor)
+               # verify if in limit
+               if 0 <= nx < self.board.width and 0 <= ny < self.board.height:
+                    neighbor = self.board.cells[ny][nx]
+                    self.neighbors.append(neighbor)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from board import Board
 
 if __name__ == "__main__" :
-    board = Board(10, 20, [])
+    board = Board(10, 20)
     board.fill_cells()
 
-    board.cell(5, 5).calcNeighbors()
-    print(board.cell(5, 5).neighbors)
+    board.cells[5][5].calc_neighbors()
+    print(board.cells[5][5].neighbors)
+
+    print(board)
