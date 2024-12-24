52ac2aad8063eaa75396528747945227fd3bbf4d
ClemouDouard <clementleveque911@gmail.com>
2024-12-24 12:06:56 +0100

    added time setting

--- src/config.py
+++ src/config.py
@@ -4,3 +4,4 @@ class Settings:
         self.width = 10
         self.height = 10
         self.seed = [(1,4), (1,5), (1,3), (2,4), (4,8), (5,9), (1,2)]
+        self.incr_time = 1
--- src/game.py
+++ src/game.py
@@ -1,5 +1,6 @@
 from board import Board
 from config import Settings
+import time
 
 class Game:
 
@@ -8,9 +9,11 @@ class Game:
         self.board.fill_cells()
         self.board.set_seed(settings.seed)
         self.nb_iter = nb_iter
+        self.incr_time = settings.incr_time
 
     def run(self) -> None:
         for i in range (self.nb_iter):
             print(self.board)
             self.board.next_board()
+            time.sleep(self.incr_time)
         print(self.board)
